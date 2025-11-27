ae587cdbc442fad9a0af0ba00fa555fb522eb610
Parker Bock <parkerdbock@gmail.com>
2025-11-26 17:10:09 -0800

    Initial commit

---
+++ Variables/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // variable = A reusable container for a value.
+    //         A variable acts like the value it contains when you call it
+    int x = 1;
+    int y = 2;
+    int sum = x + y;
+
+    std::cout << "x = 1" << '\n';
+    std::cout << "y = 2" << '\n';
+    std::cout << "x + y = " << sum;
+
+    return 0;
+}
---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int main(){
+
+    // this is a comment
+   
+    /*
+    this is a multi line comment
+    :-)
+    */
+    int var = 1;
+    std::cout << "Hello World\n";
+    std::cout << "Second Line! " << var << " Yeah thats right, that was a variable" << '\n';
+    std::cout << "Third Line!";
+    return 0;
+}
